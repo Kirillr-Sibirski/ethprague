aac5593cdf185ef57687a4ede7192bd3642d1dcf
Kirillr-Sibirski <kirill.rybkov@outlook.com>
2025-05-31 16:26:20 +0200

    Pyth implemented

--- packages/foundry/contracts/WeSplit.sol
+++ packages/foundry/contracts/WeSplit.sol
@@ -3,8 +3,12 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "forge-std/console.sol"; // Useful for debugging. Remove when deploying to a live network.
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
 
 contract WeSplit {
+    IPyth pyth;
+
     struct Contributor {
         string username; // To identify between different users
         uint256 contributed; // Amount of assets contributed so far in the requested token
@@ -15,12 +19,14 @@ contract WeSplit {
         address tokenAddress; // The address of the token that they want to receive
         uint256 fiatAmount; // The amount of fiat they have spent (probably don't even need this since we store the toContribute in each Contributor)
         string currency; // The fiat currency used to pay for the bill
+        string description;
         bool verified; // Is it vlayer verified or not
         Contributor[] contributors; // The pre-selected contributors. TODO: Maybe make this into a mapping of indexex usernames
     }
 
     mapping(bytes4 => Split) public splits; // Tracks all the split data
     mapping(address => bytes4[]) public userSplits; // Track all the splits for each requester
+    mapping(address => mapping(string => bytes32)) public priceFeeds; // All the different price feeds that we have
 
     event SplitRequested(
         address indexed requester,
@@ -38,6 +44,31 @@ contract WeSplit {
 
     event SplitContributed();
 
+    /**
+     * @param _pythAddress The address of the Pyth contract
+     */
+    constructor(
+        address _pythAddress,
+        address[] memory supportedAssets,
+        string[] memory currencies,
+        bytes32[] memory feedIds
+    ) {
+        require(
+            supportedAssets.length * currencies.length == feedIds.length,
+            "Feed ID count mismatch"
+        );
+
+        pyth = IPyth(_pythAddress);
+
+        uint256 index = 0;
+        for (uint i = 0; i < supportedAssets.length; i++) {
+            for (uint j = 0; j < currencies.length; j++) {
+                priceFeeds[supportedAssets[i]][currencies[j]] = feedIds[index];
+                index++;
+            }
+        }
+    }
+
     /// @notice Creates a new split request
     /// @dev Creates a splitId using the hashing function
     /// @param fiatAmount The amount of fiat spent on a bill
@@ -48,6 +79,7 @@ contract WeSplit {
         uint256 fiatAmount,
         address tokenAddress,
         string calldata currency,
+        string calldata description,
         Contributor[] calldata contributors
     ) external {
         require(fiatAmount > 0, "Amount must be greater than 0");
@@ -71,6 +103,7 @@ contract WeSplit {
             tokenAddress: tokenAddress,
             fiatAmount: fiatAmount,
             currency: currency,
+            description: description,
             verified: false, //TODO: Fix this once vlayer is implemented
             contributors: contributors
         });
@@ -90,7 +123,11 @@ contract WeSplit {
     /// @dev Handles both ERC-20 and native token transfers
     /// @param splitId The ID of the split to withdraw from
     /// @param withdrawAmount The amount of tokens to withdraw (in wei or token decimals)
-    function splitWithdraw(bytes4 splitId, uint256 withdrawAmount) external {
+    function splitWithdraw(
+        bytes4 splitId,
+        uint256 withdrawAmount,
+        bytes[] calldata priceUpdate
+    ) external {
         // TODO: Add the proper state handling, add requires to prevent them from withdrawing more than they actually have
         Split memory split = splits[splitId];
 
@@ -108,8 +145,6 @@ contract WeSplit {
             totalContributed <= withdrawAmount,
             "Withdrawing more tokens than has been contributed so far"
         );
-        // Calculate the price of the withdraw amount in the fiat currency
-        // Compare it to the fiatAmount, if it's the same then we remove the split entirely
 
         if (split.tokenAddress == address(0)) {
             // If native token (ETH, MATIC, etc.)
@@ -122,6 +157,33 @@ contract WeSplit {
                 token.transfer(msg.sender, withdrawAmount),
                 "ERC20 withdrawal failed"
             );
+        }
+
+        PythStructs.Price memory price = getPrice(
+            split.currency,
+            split.tokenAddress,
+            priceUpdate
+        );
+        require(price.price >= 0, "Negative price not allowed");
+        require(price.expo >= 0, "Negative exponent not allowed");
+        uint256 withdrawValue = withdrawAmount *
+            uint256(int256(price.price * price.expo));
+        uint256 lowerBound = (split.fiatAmount * 995) / 1000; // 99.5% of fiatAmount
+        uint256 upperBound = (split.fiatAmount * 1005) / 1000; // 100.5% of fiatAmount
+
+        if (withdrawValue >= lowerBound && withdrawValue <= upperBound) {
+            // TODO: pop the split from the mappings
+            delete splits[splitId];
+
+            // Remove splitId from userSplits[msg.sender] array
+            bytes4[] storage splitsArray = userSplits[msg.sender];
+            for (uint i = 0; i < splitsArray.length; i++) {
+                if (splitsArray[i] == splitId) {
+                    splitsArray[i] = splitsArray[splitsArray.length - 1]; // Replace with last element
+                    splitsArray.pop(); // Remove last element
+                    break;
+                }
+            }
         }
 
         emit SplitWithdrawn(msg.sender, splitId, withdrawAmount);
@@ -156,11 +218,19 @@ contract WeSplit {
         require(found, "Contributor not found");
     }
 
+    /// @notice this function is used to get the price of an asset against any other currency
     function getPrice(
         string memory currencyTicket,
-        address tokenAddress
-    ) private returns (uint256) {
-        // TODO: Here goes the Pyth implementation that returns the price of the asset
+        address tokenAddress,
+        bytes[] calldata priceUpdate
+    ) private returns (PythStructs.Price memory) {
+        bytes32 priceFeedId = priceFeeds[tokenAddress][currencyTicket];
+        require(priceFeedId != 0, "Unsupported asset/currency");
+
+        uint fee = pyth.getUpdateFee(priceUpdate);
+        pyth.updatePriceFeeds{value: fee}(priceUpdate);
+
+        return pyth.getPriceNoOlderThan(priceFeedId, 60);
     }
 
     // Helper for frontend to get all split IDs of a user, display it in a list
