cd1f039e4b07a16c6a8e04b11efbeece04dc5416
Konstantin M <kopamedve@gmail.com>
2025-05-31 15:06:36 +0200

    Add Split-related interfaces for handling contributions and withdrawals

--- packages/nextjs/app/api/types.ts
+++ packages/nextjs/app/api/types.ts
@@ -4,3 +4,35 @@ export interface Request {
   description: string;
   id: number;
 }
+
+export interface Split {
+  tokenAddress: string;
+  fiatAmount: number;
+  fiatCurrency: string;
+  verified: boolean;
+  requestorAddress: string;
+}
+
+export interface SplitRequest {
+  fiatAmount: number;
+  tokenAddress: string;
+  fiatCurrency: string;
+  contributors: Contributor[];
+}
+
+export interface Contributor {
+  username: string;
+  contributed: number;
+  toContribute: number;
+}
+
+export interface SplitWithdraw {
+  splitId: string;
+  withdrawAmount: number;
+}
+
+export interface ContributeToSplit {
+  splitId: string;
+  username: string;
+  contributeAmount: number;
+}
